dfc43449e69f4f5b86dcbe29800fe92bf009e594
0honus0 <63273720+0honus0@users.noreply.github.com>
2026-07-30 19:04:44 +0100

    build(deps): 补齐直接依赖并清理无用包

--- packages/backend/src/types/easyzip.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'easyzip' {
-    export class EasyZip {
-        constructor();
-        file(name: string, content: string): void;
-        writeToBuffer(options: { password: string, compress: boolean }, callback: (err: Error | null, buffer: Buffer) => void): void;
-    }
-}
